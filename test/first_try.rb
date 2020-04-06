1116250dc3ecd509a11ed5e09ee010406f378aba
brian-corpas <briancorpas@gmail.com>
2020-04-06 17:07:07 +0100

    first_try.rb

---
+++ test/first_try.rb
@@ -0,0 +1,7 @@
+puts "What's your first name?"
+first_name = gets.chomp.capitalize!
+
+puts "What's your last name?"
+last_name = gets.chomp.capitalize!
+
+puts "Your name is #{first_name} #{last_name}."
